62e53f0cf7e8e3d27b8af29d69cdde2e74309768
Weskley Bezerra <weskleymb@hotmail.com>
2018-08-23 11:09:48 -0300

    commit inicial

--- sexo/index.php
+++ sexo/index.php
@@ -28,7 +28,7 @@ $sexos = $dao->findAll();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sexos</title>
     <link rel="stylesheet" href="../assets/css/bootstrap.css">
-    <link rel="stylesheet" href="../assets/css/all.css">
+    <link rel="stylesheet" href="../assets/css/fontawesome.css">
 </head>
 <body>
     <div class="container">
