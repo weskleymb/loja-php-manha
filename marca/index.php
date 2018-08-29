4549f1983ae4ce142a4d732d530ef6c0a13f1437
Weskley Bezerra <weskleymb@hotmail.com>
2018-08-29 07:56:39 -0300

    commit inicial

--- marca/index.php
+++ marca/index.php
@@ -1,25 +1,24 @@
-<?php require_once(__DIR__ . "/../classes/modelo/Sexo.class.php"); ?>
-<?php require_once(__DIR__ . "/../classes/dao/SexoDAO.class.php"); ?>
+<?php require_once(__DIR__ . "/../classes/modelo/Marca.class.php"); ?>
+<?php require_once(__DIR__ . "/../classes/dao/MarcaDAO.class.php"); ?>
 <?php 
-$dao = new SexoDAO();
-$sexo = new Sexo();
+$dao = new MarcaDAO();
+$marca = new Marca();
 if (isset($_POST['salvar']) && $_POST['salvar'] == 'salvar') {
-    $sexo->setNome($_POST['sexo']);
-    $sexo->setSigla($_POST['sigla']);
+    $marca->setNome($_POST['marca']);
     if ($_POST['id'] != '') {
-        $sexo->setId($_POST['id']);
+        $marca->setId($_POST['id']);
     }
-    $dao->save($sexo);
+    $dao->save($marca);
     header('location: index.php');
 }
 if (isset($_POST['editar']) && $_POST['editar'] == 'editar') {
-    $sexo = $dao->findById($_POST['id']);
+    $marca = $dao->findById($_POST['id']);
 }
 if (isset($_POST['remover']) && $_POST['remover'] == 'remover') {
     $dao->remove($_POST['id']);
     header('location: index.php');
 }
-$sexos = $dao->findAll();
+$marcas = $dao->findAll();
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -35,16 +34,12 @@ $sexos = $dao->findAll();
         <div class="row" style="margin-top: 50px;">
             <div class="col-6"><!-- form -->
                 <fieldset>
-                    <legend>Dados do Sexo</legend>
+                    <legend>Dados da Marca</legend>
                     <form action="index.php" method="post">
-                        <input type="hidden" name="id" value="<?=$sexo->getId();?>">
+                        <input type="hidden" name="id" value="<?=$marca->getId();?>">
                         <div class="form-group">
-                            <label for="sexo">Sexo</label>
-                            <input type="text" class="form-control" name="sexo" id="sexo" maxlength="12" required value="<?=$sexo->getNome();?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="sigla">Sigla</label>
-                            <input type="text" class="form-control" name="sigla" id="sigla" maxlength="1" required value="<?=$sexo->getSigla();?>">
+                            <label for="marca">Marca</label>
+                            <input type="text" class="form-control" name="marca" id="marca" maxlength="12" required value="<?=$marca->getNome();?>">
                         </div>
                         <div class="form-group">
                             <button type="submit" class="btn btn-primary btn-block" name="salvar" value="salvar">
@@ -56,31 +51,29 @@ $sexos = $dao->findAll();
             </div>
             <div class="col-6"><!-- table -->
                 <fieldset>
-                    <legend>Lista de Sexos</legend>
+                    <legend>Lista de Marcas</legend>
                     <table class="table table-striped table-hover">
                         <thead>
                             <tr>
                                 <th>#</th>
-                                <th>sexo</th>
-                                <th>sigla</th>
+                                <th>marca</th>
                                 <th colspan="2">ações</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <?php foreach($sexos as $sexo): ?>
+                            <?php foreach($marcas as $marca): ?>
                                 <tr>
-                                    <td><?=$sexo->getId();?></td>
-                                    <td><?=$sexo->getNome();?></td>
-                                    <td><?=$sexo->getSigla();?></td>
+                                    <td><?=$marca->getId();?></td>
+                                    <td><?=$marca->getNome();?></td>
                                     <td>
                                         <form action="index.php" method="post">
-                                            <input type="hidden" name="id" value="<?=$sexo->getId();?>">
+                                            <input type="hidden" name="id" value="<?=$marca->getId();?>">
                                             <button type="submit" class="btn btn-sm btn-success" name="editar" value="editar"><i class="fas fa-edit"></i></button>
                                         </form>
                                     </td>
                                     <td>
                                         <form action="index.php" method="post">
-                                            <input type="hidden" name="id" value="<?=$sexo->getId();?>">
+                                            <input type="hidden" name="id" value="<?=$marca->getId();?>">
                                             <button type="submit" class="btn btn-sm btn-danger" name="remover" value="remover"><i class="fas fa-trash-alt"></i></button>
                                         </form>
                                     </td>
