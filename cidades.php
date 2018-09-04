b3edfc59fd3826cf489aa82fbc593d9826cc15b4
Weskley Bezerra <weskleymb@hotmail.com>
2018-09-04 10:37:48 -0300

    brincando com ajax

--- cidades.php
+++ cidades.php
@@ -9,23 +9,23 @@ $uf->setId($uf_id);
 $dao = new CidadeDAO();
 $cidades = $dao->findByUnidadeFederativa($uf);
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cidades</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-</head>
-<body>
-    <label for="cidade">Cidade</label>
-    <select class="form-control" name="cidade" id="cidade">
-        <option value="0">--SELECIONE--</option>
-        <?php foreach($cidades as $cidade): ?>
-            <option value="<?=$cidade->getId();?>">
-                <?=$cidade->getNome();?>
-            </option>
-        <?php endforeach; ?>
-    </select>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cidades</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+    </head>
+    <body>
+        <label for="cidade">Cidade</label>
+        <select class="form-control" name="cidade" id="cidade">
+            <option value="0" selected disabled>--SELECIONE--</option>
+            <?php foreach($cidades as $cidade): ?>
+                <option value="<?=$cidade->getId();?>">
+                    <?=$cidade->getNome();?>
+                </option>
+            <?php endforeach; ?>
+        </select>
+    </body>
 </html>
--- enderecos.php
+++ enderecos.php
@@ -6,47 +6,47 @@ $dao = new UnidadeFederativaDAO();
 $ufs = $dao->findAll();
 
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo Ajax</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-</head>
-<body>
-    <div class="container" style="margin-top: 50px;">
-        <form>
-            <div class="col-12 form-group" id="div_uf"><!-- select estado -->
-                <label for="uf">Estado</label>
-                <select class="form-control" name="uf" id="uf" onchange="show_cidades(this.value);">
-                    <option value="0" selected disabled>--SELECIONE--</option>
-                    <?php foreach($ufs as $uf): ?>
-                        <option value="<?=$uf->getId();?>">
-                            <?=$uf->getNome();?>
-                        </option>
-                    <?php endforeach; ?>
-                </select>
-            </div>
-            <div class="col-12 form-group" id="div_cidade"><!-- select cidade -->
-                <label for="cidade">Cidade</label>
-                <select class="form-control" name="cidade" id="cidade">
-                    <option value="0">--SELECIONE--</option>
-                </select>
-            </div>
-            <div class="col-12 form-group" id="div_bairro"><!-- select bairro -->
-                <label for="bairro">Bairro</label>
-                <select class="form-control" name="bairro" id="bairro">
-                    <option value="0">--SELECIONE--</option>
-                </select>
-            </div>
-            <div class="col-12 form-group"><!-- select botao -->
-                <button type="submit" class="btn btn-danger btn-block">
-                    Enviar
-                </button>
-            </div>
-        </form>
-    </div>
-    <script src="assets/js/ajax_enderecos.js"></script>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Exemplo Ajax</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+    </head>
+    <body>
+        <div class="container" style="margin-top: 50px;">
+            <form>
+                <div class="col-12 form-group" id="div_uf"><!-- select estado -->
+                    <label for="uf">Estado</label>
+                    <select class="form-control" name="uf" id="uf" onchange="show_cidades(this.value);">
+                        <option value="0" selected disabled>--SELECIONE--</option>
+                        <?php foreach($ufs as $uf): ?>
+                            <option value="<?=$uf->getId();?>">
+                                <?=$uf->getNome();?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+                <div class="col-12 form-group" id="div_cidade"><!-- select cidade -->
+                    <label for="cidade">Cidade</label>
+                    <select class="form-control" name="cidade" id="cidade">
+                        <option value="0" selected disabled>--SELECIONE--</option>
+                    </select>
+                </div>
+                <div class="col-12 form-group" id="div_bairro"><!-- select bairro -->
+                    <label for="bairro">Bairro</label>
+                    <select class="form-control" name="bairro" id="bairro">
+                        <option value="0">--SELECIONE--</option>
+                    </select>
+                </div>
+                <div class="col-12 form-group"><!-- select botao -->
+                    <button type="submit" class="btn btn-danger btn-block">
+                        Enviar
+                    </button>
+                </div>
+            </form>
+        </div>
+        <script src="assets/js/ajax_enderecos.js"></script>
+    </body>
 </html>
